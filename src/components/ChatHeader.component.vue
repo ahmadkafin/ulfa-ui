90b3affb464ed085b4d554e75c65633f14428adf
Ahmad Kafin <kafinahmad@gmail.com>
2026-09-17 08:37:12 +0700

    change icon on header

--- src/components/ChatHeader.component.vue
+++ src/components/ChatHeader.component.vue
@@ -4,7 +4,7 @@
       <div class="relative">
         <div class="status-indicator-wrapper">
           <img
-            src="/src/assets/Ulfabot-1-front.png"
+            src="/src/assets/Ulfa-2-front-face.png"
             alt="Ulfa Avatar"
             class="w-full h-full rounded-full object-cover"
           />
